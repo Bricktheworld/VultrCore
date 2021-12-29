48aa41e8dc1efcb8727bd80c68962dced6da17da
Bricktheworld <brandon.shihabi@gmail.com>
2021-12-28 22:45:35 -0800

    Added simple tests for error and error_or

--- tests/types/error.cpp
+++ tests/types/error.cpp
@@ -1,3 +1,21 @@
-//
-// Created by brandon on 12/28/21.
-//
+#include <gtest/gtest.h>
+#include <types/error.h>
+
+using namespace Vultr;
+TEST(Error, Create)
+{
+	{
+		auto err = Error();
+		ASSERT_TRUE(err);
+	}
+	{
+		auto err = Error("Some message");
+		ASSERT_STRCASEEQ(err.message, "Some message");
+		ASSERT_EQ(err.code, -1);
+	}
+	{
+		auto err = Error("Some message", 20);
+		ASSERT_STRCASEEQ(err.message, "Some message");
+		ASSERT_EQ(err.code, 20);
+	}
+}
--- tests/types/error_or.cpp
+++ tests/types/error_or.cpp
@@ -1,3 +1,34 @@
-//
-// Created by brandon on 12/28/21.
-//
+#include <gtest/gtest.h>
+#include <types/error_or.h>
+
+using namespace Vultr;
+TEST(ErrorOr, Create)
+{
+	{
+		ErrorOr<u32> result = Error("An error", -1);
+		bool worked         = false;
+		if check (result, auto number, auto err)
+		{
+			worked = false;
+		}
+		else
+		{
+			worked = true;
+		}
+		ASSERT_TRUE(worked);
+	}
+	{
+		ErrorOr<u32> result = 20;
+		bool worked         = false;
+		if check (result, auto number, auto err)
+		{
+			worked = true;
+			ASSERT_EQ(number, 20);
+		}
+		else
+		{
+			worked = false;
+		}
+		ASSERT_TRUE(worked);
+	}
+}
